5b952c8d7853eb3c6d29eeb260694673f9fc07ae
tiunov2008 <tiunov2008.va@gmail.com>
2020-08-05 11:14:52 +0300

    create: google map

--- js/main.js
+++ js/main.js
@@ -1,75 +1,109 @@
 $(document).ready(function () {
-    new Swiper(".hotel-slider", {
-        loop: !0,
+    var hotelSlider = new Swiper(".hotel-slider", {
+        // Optional parameters
+        loop: true,
+
+        // Navigation arrows
         navigation: {
             nextEl: ".hotel-slider__button_next",
             prevEl: ".hotel-slider__button_prev",
         },
-        keyboard: { enabled: !0, onlyInViewport: !1 },
-    }),
-        new Swiper(".reviews-slider", {
-            loop: !0,
-            navigation: {
-                nextEl: ".reviews-slider__button_next",
-                prevEl: ".reviews-slider__button_prev",
-            },
-            keyboard: { enabled: !0, onlyInViewport: !1 },
-        });
-    ymaps.ready(function () {
-        new ymaps.Map("map", {
+
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
+    });
+
+    var reviewsSlider = new Swiper(".reviews-slider", {
+        // Optional parameters
+        loop: true,
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".reviews-slider__button_next",
+            prevEl: ".reviews-slider__button_prev",
+        },
+
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
+    });
+
+    ymaps.ready(init);
+    function init() {
+        var myMap = new ymaps.Map("map", {
             center: [25.078704097883328, 55.135232499999944],
             zoom: 15,
             controls: ["zoomControl"],
         });
-    }),
-        $(".menu-button").on("click", function () {
-            $(".navbar-bottom").toggleClass("navbar-bottom_visible");
-        });
-    var e = $("[data-toggle='modal']");
-    function o() {
-        event.preventDefault();
-        var e = $(".modal__overlay"),
-            o = $(".modal__dialog");
-        e.removeClass("modal__overlay_visible"),
-            o.removeClass("modal__dialog_visible");
     }
-    $(".modal__close").on("click", o),
-        e.on("click", function () {
-            var e = $(this).attr("data-href");
-            $(e).find(".modal__overlay").addClass("modal__overlay_visible"),
-                $(e).find(".modal__dialog").addClass("modal__dialog_visible");
-        }),
-        $(document).keyup(function (e) {
-            ("Escape" !== e.key && 27 !== e.keyCode) || o();
-        }),
-        $(".form").each(function () {
-            $(this).validate({
-                errorClass: "invalid",
-                errorElement: "span",
-                messages: {
-                    name: {
-                        required: "Please enter your name",
-                        minlength: "The name must be 2 letters or more.",
-                    },
-                    email: {
-                        required: "We need your email address to contact you",
-                        email:
-                            "Your email address must be in the format of example@domain.com",
-                    },
-                    phone: { required: "Telephone number is required" },
-                },
-            });
-        }),
-        $(".newsletter__subscribe").validate({
-            errorClass: "invalid-email",
+
+    var menuButton = $(".menu-button");
+
+    menuButton.on("click", function () {
+        $(".navbar-bottom").toggleClass("navbar-bottom_visible");
+    });
+
+    var modalButton = $("[data-toggle='modal']");
+    var closeButton = $(".modal__close");
+    closeButton.on("click", closeModal);
+    modalButton.on("click", openModal);
+    $(document).keyup(function (e) {
+        if (e.key === "Escape" || e.keyCode === 27) {
+            closeModal();
+        }
+    });
+
+    function openModal() {
+        var targeModal = $(this).attr("data-href");
+        $(targeModal)
+            .find(".modal__overlay")
+            .addClass("modal__overlay_visible");
+        $(targeModal).find(".modal__dialog").addClass("modal__dialog_visible");
+    }
+
+    function closeModal() {
+        event.preventDefault();
+        var modalOverlay = $(".modal__overlay");
+        var modalDialog = $(".modal__dialog");
+        modalOverlay.removeClass("modal__overlay_visible");
+        modalDialog.removeClass("modal__dialog_visible");
+    }
+
+    $(".form").each(function () {
+        $(this).validate({
+            errorClass: "invalid",
+            errorElement: "span",
             messages: {
+                name: {
+                    required: "Please enter your name",
+                    minlength: "The name must be 2 letters or more.",
+                },
                 email: {
                     required: "We need your email address to contact you",
                     email:
                         "Your email address must be in the format of example@domain.com",
                 },
+                phone: {
+                    required: "Telephone number is required",
+                },
             },
-        }),
-        $("[type='tel']").mask("+7 (999) 999-99-99"),
-        AOS.init();
+        });
+    });
+    $(".newsletter__subscribe").validate({
+        errorClass: "invalid-email",
+        messages: {
+            email: {
+                required: "We need your email address to contact you",
+                email:
+                    "Your email address must be in the format of example@domain.com",
+            },
+        },
+    });
+
+    $("[type='tel']").mask("+7 (999) 999-99-99");
+
+    AOS.init();
 });
